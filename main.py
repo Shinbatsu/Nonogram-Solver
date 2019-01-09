f31e28130d9edb8bae00317e8ac38dcc460913a1
Shinbatsu <nojic132@gmail.com>
2019-01-09 17:01:59 +0300

    test example and display function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 
+def solve(clues):
+    ...
+    
 def cycle(board, row_clues, column_clues, height, width, total_cells):
     prev_count = -1
     iteration = 0
@@ -21,3 +24,23 @@ def cycle(board, row_clues, column_clues, height, width, total_cells):
         rows_to_update, cols_to_update = updated_rows, set()
 
     return solved_cells, iteration
+
+def show_board(board):
+    for row in board:
+        print(''.join(row))
+
+if __name__ == "__main__":
+    result = solve((
+        (
+            (1, 1, 3), (3, 2, 1, 3), (2, 2), (3, 6, 3), (3, 8, 2), (15,), (8, 5), (15,),
+            (7, 1, 4, 2), (7, 9), (6, 4, 2), (2, 1, 5, 4), (6, 4), (2, 6), (2, 5),
+            (5, 2, 1), (6, 1), (3, 1), (1, 4, 2, 1), (2, 2, 2, 2)
+        ),
+        (
+            (2, 1, 1), (3, 4, 2), (4, 4, 2), (8, 3), (7, 2, 2), (7, 5), (9, 4), (8, 2, 3),
+            (7, 1, 1), (6, 2), (5, 3), (3, 6, 3), (2, 9, 2), (1, 8), (1, 6, 1), (3, 1, 6),
+            (5, 5), (1, 3, 8), (1, 2, 6, 1), (1, 1, 1, 3, 2)
+        )
+    ))
+
+    show_board(result)
